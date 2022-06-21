60c02b052fdb028d8e22fd475c2bb45106a3c575
Kushnov <kushnov@inbox.ru>
2022-06-21 17:18:54 +0300

    Homework 7

---
+++ src/main/java/Homework_7/Main.java
@@ -0,0 +1,9 @@
+package Homework_7;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        userInterface.runApplication();
+    }
+}
+
